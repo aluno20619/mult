b35016b0342940e3a7fcbc96092670ada188c958
claudia <aluno20619@ipt.pt>
2019-05-30 15:19:25 +0100

    Add project files.

---
+++ Multas/Models/Multas.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Multas.Models {
+   public class Multas {
+
+      public int ID { get; set; }
+
+      public string Infracao { get; set; }
+
+      [Display(Name ="Local da multa")]
+      public string LocalDaMulta { get; set; }
+
+      public decimal ValorMulta { get; set; }
+
+      [Display(Name ="Data da aplicação da multa")]
+      public DateTime DataDaMulta { get; set; }
+
+      // **************************************
+      // criação das Chaves Forasteiras
+      // **************************************
+
+      // FK para Viatura
+      [ForeignKey("Viatura")]
+      public int ViaturaFK { get; set; }  // Base de Dados
+      public virtual Viaturas Viatura { get; set; }  // C#
+
+      // FK para Condutor
+      [ForeignKey("Condutor")]
+      public int CondutorFK { get; set; }
+      public virtual Condutores Condutor { get; set; }
+
+      // FK para Agente
+      [ForeignKey("Agente")]
+      public int AgenteFK { get; set; }
+      public virtual Agentes Agente { get; set; }
+
+
+   }
+
+
+}
+
+
+
+
---
+++ Multas/Models/Viaturas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Multas.Models {
+   public class Viaturas {
+
+      public int ID { get; set; }
+
+      public string Matricula { get; set; }
+
+      public string Marca { get; set; }
+
+      public string Modelo { get; set; }
+
+      public string Cor { get; set; }
+
+      public string NomeDono { get; set; }
+
+      public string MoradaDono { get; set; }
+
+      public string CodPostalDono { get; set; }
+
+      // *************************************
+      // lista das multas associadas à Viatura
+      public ICollection<Multas> ListaDeMultas { get; set; }
+
+
+   }
+}
